4c8cebf0ab3df7732d9b1e53913219cfc718f517
Jaehoon Lee <alphahoon@gmail.com>
2016-05-30 23:45:24 +0900

    login view added

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var index = require('./routes/index');
+var login = require('./routes/login');
 var users = require('./routes/users');
 
 var app = express();
@@ -19,7 +20,6 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
-
 
 // SQL Test Codes
 /*
@@ -49,7 +49,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', index);
+app.use('/login', login);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -82,6 +83,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
