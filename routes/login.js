93bdf2735bf20de4048ff29374f48b11261bf7c4
heewoonam <heewoo.nam@kaist.ac.kr>
2016-06-01 19:36:16 +0900

    change layout

--- routes/login.js
+++ routes/login.js
@@ -14,7 +14,7 @@ router.post('/', function (req, res, next) {
   var query = db.connection.query('select * from User where userid=' + db.mysql.escape(req.body.id),
     function (err, rows) {
       //console.log(rows);
-      if (rows[0] && req.body.id == rows[0].userid && req.body.pswd === rows[0].Password)
+      if (rows && rows[0] && req.body.id == rows[0].userid && req.body.pswd === rows[0].Password)
         req.session.regenerate(function () {
           req.session.logined = true;
           req.session.user_id = req.body.id;
