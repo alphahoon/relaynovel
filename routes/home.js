2cef0546727961743062952ad27229f8bf1ad774
urmsori <urmsori@gmail.com>
2016-06-03 00:03:02 +0900

    commit error 해결

--- routes/home.js
+++ routes/home.js
@@ -6,14 +6,10 @@ var db = require('../database.js');
 router.get('/', function (req, res, next) {
     if (!req.session.logined)
         res.redirect('/');
-<<<<<<< HEAD
     console.log(req.session.user_id);
     db.get_recentupdate(function (callback) {
         console.log(callback);
     });
-=======
-    // console.log(req.session.user_id);
->>>>>>> 192f294e24fa3aad0e7703cc3ec7315818e8dd22
     res.render('home', { session: req.session });
 });
 
