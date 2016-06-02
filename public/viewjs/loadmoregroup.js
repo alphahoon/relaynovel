23247b543c6c295cab53063979edf4a5a67e5fec
JinYeob Kim <urmsori@golfzon.com>
2016-06-02 19:24:23 +0900

    ajax 마스터 by 김진엽

--- public/viewjs/loadmoregroup.js
+++ public/viewjs/loadmoregroup.js
@@ -1,17 +1,35 @@
 var div = document.getElementById("cgroup");
-var groups = JSON.parse(strgroups);
-
-groups.forEach(function(group) {
-div.innerHTML += `
+var nowIndex = 0;
+var moreNum = 4;
+//-----------------------------------------------------------------------
+// 2) Send a http request with AJAX http://api.jquery.com/jQuery.ajax/
+//-----------------------------------------------------------------------
+$.ajax({
+    url: '/joinable',                  //the script to call to get data          
+    data: {
+        start: nowIndex,
+        num: moreNum
+    },                        //you can insert url argumnets here to pass
+    type: 'post',
+    dataType: 'json',                //data format      
+    success: function (groups)          //on recieve of reply
+    {
+        if (groups.length > 0) {
+            nowIndex += groups.length;
+            groups.forEach(function (group) {
+                div.innerHTML += `
 <li class="collection-item avatar">
-    <img src="`+ group.GroupImageURL +`" alt="" class="circle">
-    <span class="title">`+group.Groupname+`</span>
+    <img src="`+ group.GroupImageURL + `" alt="" class="circle">
+    <span class="title">`+ group.Groupname + `</span>
     <p>
-    Genre : `+group.Genre+`
+    Genre : `+ group.Genre + `
     <br>
     Second Line
     </p>
     <a href="#!" class="secondary-content"><i class="medium material-icons">view_module</i></a>
-</li>`;
-    
-}, this);
+</li>
+`;
+            });
+        }
+    }
+});
--- routes/joinable.js
+++ routes/joinable.js
@@ -2,27 +2,26 @@ var express = require('express');
 var router = express.Router();
 var db = require('../database.js');
 
-/* GET home page. */
+/* GET Joinable page */
 router.get('/', function (req, res, next) {
     // if (!req.session.logined)
     //     res.redirect('/');
 
+    res.render('joinable', {
+        session: req.session
+    });
+});
+
+/* Ajax */
+router.post('/', function (req, res, next) {
     var query = db.connection.query(
-        'select * from RenoGroup',
+        'select * from RenoGroup Limit '+ req.body.start + ', '+ req.body.num,
         function (err, rows) {
             console.log(rows);
             if (rows) {
-                res.render('joinable', {
-                    groups: rows,
-                    session: req.session
-                });
-            }
-            else {
-                console.log("dberror");
-                res.redirect('/');
+                res.send(rows);
             }
         });
 
 });
-
 module.exports = router;
