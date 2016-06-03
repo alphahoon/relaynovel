627e5e45ecbb986d466b83b8751cd5ad8a578ad0
heewoonam <heewoo.nam@kaist.ac.kr>
2016-06-03 16:47:00 +0900

    modulize file upload

--- routes/register.js
+++ routes/register.js
@@ -4,6 +4,7 @@ var db = require('../database.js');
 var formidable = require('formidable');
 var fs = require('fs');
 var path = require('path');
+var file = require('../file.js')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -13,50 +14,28 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-    var form = new formidable.IncomingForm();
-    // 파일 전송 시 첫번째 chunk가 전달되면 호출
-    form.keepExtensions = true;
-    form.on('fileBegin', function (name, file) {
-        console.log('fileBegin - ' + name + ':' + JSON.stringify(file));
-    })
-        // 파일의 chunk가 전달될 때 마다 호출
-        .on('progress', function (bytesReceived, bytesExpected) {
-            console.log('progress: ' + bytesReceived + '/' + bytesExpected);
-        })
-        // 파일 전송도중 esc나 페이지 전환 등으로 중단될 때 호출
-        .on('aborted', function () {
-            console.log('aborted');
-        })
-        // error 발생 시 호출
-        .on('error', function () {
-            console.log('error');
-        })
-        // 파일 전송이 끝난 경우 호출
-        .on('end', function () {
-            console.log('end');
-        });
-    form.parse(req, function (err, fields, file) {
-        if (!(fields.id && fields.name && fields.pswd1 && fields.pswd2)) res.render('register', { message: '필드를 다 채우세요.' });
-        else if (fields.pswd1 != fields.pswd2) res.render('register', {message : '패스워드가 일치하지 않습니다.'});
-        else if (fields.id.length < 4 || fields.id.length > 16) res.render('register', {message : 'ID는 4~16자 이내로 작성 바랍니다.'});
-        else if (fields.name.length < 4 || fields.name.length > 16) res.render('register', {message : '닉네임은 4~16자 이내로 작성 바랍니다.'});
+    file.upload_file (req, function (entries) {
+        if (!(entries.fields.id && entries.fields.name && entries.fields.pswd1 && entries.fields.pswd2)) res.render('register', { message: '필드를 다 채우세요.' });
+        else if (entries.fields.pswd1 != entries.fields.pswd2) res.render('register', {message : '패스워드가 일치하지 않습니다.'});
+        else if (entries.fields.id.length < 4 || entries.fields.id.length > 16) res.render('register', {message : 'ID는 4~16자 이내로 작성 바랍니다.'});
+        else if (entries.fields.name.length < 4 || entries.fields.name.length > 16) res.render('register', {message : '닉네임은 4~16자 이내로 작성 바랍니다.'});
         else {
-            check_duplicate(fields.id, fields.name, function (err) {
+            check_duplicate(entries.fields.id, entries.fields.name, function (err) {
                 if (err) {
                     res.render('register', { message: err });
                 }
                 else {
                     var profile_pic;
-                    if (file.profile.name) {
-                        var new_path = __dirname + '/../public/userimages/' + fields.id + path.extname(file.profile.path);
-                        fs.rename(file.profile.path, new_path);
-                        profile_pic = 'userimages/' + fields.id + path.extname(file.profile.path);
+                    if (entries.file.profile.name) {
+                        var new_path = __dirname + '/../public/userimages/' + entries.fields.id + path.extname(entries.file.profile.path);
+                        fs.rename(entries.file.profile.path, new_path);
+                        profile_pic = 'userimages/' + entries.fields.id + path.extname(entries.file.profile.path);
                     }
                     else profile_pic = "userimages/empty_user.jpg";
                     var post = {
-                        userid : fields.id,
-                        Nickname : fields.name,
-                        Password : fields.pswd1,
+                        userid : entries.fields.id,
+                        Nickname : entries.fields.name,
+                        Password : entries.fields.pswd1,
                         Admin : false,
                         Blocked : false,
                         Profilepic : profile_pic
@@ -67,7 +46,7 @@ router.post('/', function (req, res, next) {
                     });                    
                 }
             });
-        }
+        }        
     });
 });
 
