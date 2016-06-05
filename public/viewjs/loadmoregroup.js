110345c033c87ececc2a35b4efc68e2b2a09a74f
alphahoon <alphahoon@kaist.ac.kr>
2016-06-06 01:40:15 +0900

    dropdown menu added.

--- public/viewjs/loadmoregroup.js
+++ public/viewjs/loadmoregroup.js
@@ -18,7 +18,7 @@ function loadmore(loadnum) {
             if (groups.length > 0) {
                 nowIndex += groups.length;
                 groups.forEach(function (group) {
-                    div.innerHTML += '<li class="collection-item avatar" style="padding-left:90px; padding-right:60px; text-align:left;">    <img src="'
+                    div.innerHTML += '<li class="collection-item avatar" style="padding-left:90px; padding-right:60px; text-align:left;"> <img src="'
                     + group.GroupImageURL + '" alt="" class="circle responsive-img" style="margin:auto; width:65px; height:65px;"> <span class="title"><strong>'
     + group.Groupname + '</strong></span><p>Genre : '+ group.Genre + '<br>Second Line</p><a href="group?groupname=' + group.Groupname + '"class="secondary-content" style="margin:auto;"><i class="fa fa-sign-in fa-3x" aria-hidden="true"></i></a></li>';
                 });
