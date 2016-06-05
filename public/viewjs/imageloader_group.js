6cd4347a2357dbc867630806688aaa9576842b91
alphahoon <alphahoon@kaist.ac.kr>
2016-06-05 22:35:17 +0900

    input form style chged.

---
+++ public/viewjs/imageloader_group.js
@@ -0,0 +1,19 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#img_profile').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#upload_profile").change(function () {
+    readURL(this);
+});
+
+var control = $("#upload_profile");
+$("#delete_file").on("click", function () {
+    control.replaceWith(control = control.clone(true));
+    $('#img_profile').attr('src', 'groupimages/empty_group.jpg');
+});
