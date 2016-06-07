cc19adcaac6bf47b62023b9adb13a5fa0d84bac0
urmsori <urmsori@gmail.com>
2016-06-08 05:20:58 +0900

    작성, 투표

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,25 +6,41 @@ var fs = require('fs');
 /* GET home page. */
 
 router.post('/moregroup', function (req, res, next) {
-  if (!req.session.logined)
-  { res.redirect('/'); next('router'); }
-  else next();
+    if (!req.session.logined)
+    { res.redirect('/'); next('router'); }
+    else next();
 }, function (req, res, next) {
     db.connection.query(
-        'SELECT * from RenoGroup where Groupname in (SELECT Groupname from JoinGroup where userid= '
+        'SELECT RenoGroup.* from RenoGroup where Groupname in (SELECT Groupname from JoinGroup where userid= '
         + db.mysql.escape(req.session.user_id) + ') Limit ' + req.body.start + ', ' + req.body.num,
         function (err, rows) {
-            //console.log(rows);
-            if (rows) {
-                for (var i = 0; i < rows.length; i++) {
-                    rows[i].dispvote = 'none';
-                    rows[i].dispwrite = 'block';
-                    rows[i].dispupdate = 'block';
-                }
-                res.send(rows);
-            }
+            req.groupinfo = rows;
+            if (err && rows.length == 0) next('router');
+            else next();
         });
-})
+}, function (req, res, next) {
+    var idx = 0;
+    req.groupinfo.forEach(function(group) {
+        db.connection.query('SELECT * from Vote where Vote.VoteStatus = true and Vote.Group_GroupId = ' 
+        + db.mysql.escape(group.Groupname) ,function(err, votes) {
+            if(votes.length>0)
+                group.votestat = true;
+            else
+                group.votestat = false;
+            idx++;
+            if(idx == req.groupinfo.length)
+                next();
+        });
+    }, this);
+}, function(req, res, next) {    
+    for (var i = 0; i < req.groupinfo.length; i++) {
+        req.groupinfo[i].dispvote = req.groupinfo[i].votestat ? 'block' : 'none';
+        req.groupinfo[i].dispwrite = req.groupinfo[i].writer == req.session.user_id?'block':'none';
+        req.groupinfo[i].dispupdate = 'none';
+    }
+    res.send(req.groupinfo);
+}
+)
 
 router.get('/dashboardcard', function (req, res, next) {
     fs.readFile(__dirname + '/../public/fakehtmls/dashboardcard.html', 'utf8', function (err, data) {
@@ -36,9 +52,9 @@ router.get('/dashboardcard', function (req, res, next) {
 });
 
 router.get('/', function (req, res, next) {
-  if (!req.session.logined)
-  { res.redirect('/'); next('router'); }
-  else next();
+    if (!req.session.logined)
+    { res.redirect('/'); next('router'); }
+    else next();
 }, function (req, res, next) {
     res.render('dashboard', { session: req.session });
 });
